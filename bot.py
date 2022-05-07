445278a9d28d2f884a41e57c67a993a3bf67dcc7
steelthedev <akinwumikaliyanu@gmail.com>
2022-05-07 10:29:51 +0100

    modified

--- bot.py
+++ bot.py
@@ -63,8 +63,7 @@ def main():
 
     updater.start_webhook(listen="0.0.0.0",
                             port=int(PORT),
-                            url_path=bot_token)
-    updater.bot.setWebhook('https://oatbott.herokuapp.com/' + bot_token)
+                            url_path=bot_token, webhook_url='https://oatbott.herokuapp.com/' + bot_token)
     #updater.start_polling()
 
     updater.idle()
